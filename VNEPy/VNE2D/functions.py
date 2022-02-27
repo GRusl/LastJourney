72667a4e9da0c21392c3b58314b6970f51c109f3
Emil <emil12.05.2005@gmail.com>
2022-02-27 21:04:17 +0300

    Project_Collaider_Ruslan

--- VNEPy/VNE2D/functions.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-def init_windows(self, flags=0):
-    pygame.quit()
-    pygame.init()
-
-    self.screen = pygame.display.set_mode((0, 0) if flags == pygame.FULLSCREEN else (1000, 600), flags=flags)
